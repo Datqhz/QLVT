7209fa3d6a3d7c622108a4f55643680cc6ebc20b
Datqhz <hodat2215@gmail.com>
2022-11-24 22:20:29 +0700

    abc
    thêm tìm kiếm trong kho

--- src/main/java/com/dat/MainForm/ShowWare.java
+++ src/main/java/com/dat/MainForm/ShowWare.java
@@ -11,7 +11,7 @@ import javax.swing.table.DefaultTableModel;
 public class ShowWare extends javax.swing.JPanel {
     List<Product> list = new ArrayList() ;
     DefaultTableModel tblModelSP = new DefaultTableModel();
-    
+    private Product temp;
     public ShowWare() {
         initComponents();
         setOpaque(false);
@@ -46,13 +46,19 @@ private void fillToTable(){
         }
         model.fireTableDataChanged();
     }   
-
+    public String chuanhoaMa(String ma){
+        return ma.replaceAll(" ", "").toUpperCase();
+    }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tblSP = new javax.swing.JTable();
+        lblMaVT = new javax.swing.JLabel();
+        btnsearch = new javax.swing.JButton();
+        txtsearch = new javax.swing.JTextField();
+        lblThongBao = new javax.swing.JLabel();
 
         tblSP.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -67,6 +73,16 @@ private void fillToTable(){
         ));
         jScrollPane1.setViewportView(tblSP);
 
+        lblMaVT.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lblMaVT.setText("Mã vật tư:");
+
+        btnsearch.setIcon(new javax.swing.ImageIcon(getClass().getResource("/search.png"))); // NOI18N
+        btnsearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnsearchActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -75,19 +91,67 @@ private void fillToTable(){
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 988, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(lblMaVT, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtsearch, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(52, 52, 52)
+                        .addComponent(btnsearch, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lblThongBao, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 553, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblMaVT, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtsearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnsearch, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblThongBao, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 448, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnsearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsearchActionPerformed
+        if(chuanhoaMa(txtsearch.getText()).equals("")){
+            lblThongBao.setText("*Vui lòng nhập mã sản phẩm cần tìm!");
+            lblThongBao.setForeground(new java.awt.Color(255, 51, 51));
+        }else{
+            
+            try{
+            ProductDAO dao = new ProductDAO();
+            temp = dao.findProductByMa(chuanhoaMa(txtsearch.getText()));
+            if(temp==null){
+                lblThongBao.setText("*Sản phẩm không tồn tại.");
+                lblThongBao.setForeground(new java.awt.Color(255, 51, 51));
+            }else{
+                lblThongBao.setText("");
+               int row = dao.selectRowTable(chuanhoaMa(txtsearch.getText().toUpperCase()));
+               tblSP.setRowSelectionInterval(row,row);
+               txtsearch.setText("");
+               btnsearch.setEnabled(true);
+            }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            
+        }
+    }//GEN-LAST:event_btnsearchActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnsearch;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblMaVT;
+    private javax.swing.JLabel lblThongBao;
     private javax.swing.JTable tblSP;
+    private javax.swing.JTextField txtsearch;
     // End of variables declaration//GEN-END:variables
 }
